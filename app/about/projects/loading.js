60188cf37d1210ec2215d6e76a2bf14379807bd1
AnilBASARAN <nlbsrn@gmail.com>
2024-10-25 15:58:24 +0300

    loading

---
+++ app/about/projects/loading.js
@@ -0,0 +1,14 @@
+export default function Loading(){
+    return(
+        <div className="p-20">
+        <h1 className="mb-8">Projects</h1>
+        <ul className="space-y-12">
+          {Array(5).fill(0).map( (_el,index) => (
+            <li className="mb-4" key={index}>
+             <div className="w-full h-24 animate-pulse bg-neutral-100 dark:bg-neutral-700" ></div>
+            </li>
+          ))}
+        </ul>
+        </div>
+    );
+};
