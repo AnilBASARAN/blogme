4dad21c2a517fe804582c4a794d0b96463cc11ba
AnilBASARAN <nlbsrn@gmail.com>
2024-10-25 20:37:37 +0300

    adding a fake chatbot

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './components/header'
+import Chatbot from './components/Chatbot'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         <main>
           {children}
         </main>
+        <Chatbot />
       </body>
     </html>
   )
